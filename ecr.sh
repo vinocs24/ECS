cf6eb61afcd4d1d1d8a13d92454e20a0603a31bd
vinocs24 <47525526+vinocs24@users.noreply.github.com>
2020-01-13 16:56:03 +0530

    Delete ecr.sh

--- ecr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t myapp .
-docker tag myapp:latest 821731102189.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
-docker push 821731102189.dkr.ecr.us-east-1.amazonaws.com/myapp:latest
